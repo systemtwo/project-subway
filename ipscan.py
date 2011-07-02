3792488b3f3ee0f223880d9687f92ef5fa985584
Baron VonHolst <johnsmith@john-smiths-macbook.local>
2011-07-01 20:35:39 -0600

    Finished Sanitize function

--- ipscan.py
+++ ipscan.py
@@ -4,6 +4,7 @@
 #Store in this format (IP, time) so that old entries (1-2wks) Can be removed
 
 import socket
+import time
 
 def add_todo(iplist):
 	"""Adds Ip/Date Tuple to database FOR SCANNING FOR IP"""
@@ -14,10 +15,30 @@ def add_todo(iplist):
 def sanitize_todo():
 	"""Clean up entries older than 2 weeks
 	Will Also clean duplicates of entries"""
+
+	#Get list from file
 	f = open("todoip.txt", "r")
+	biglist = []
 	for line in f:
 		minilist = line.split(",")
-		b
+		biglist.append((minilist[0], minilist[1]))
+	
+	#Remove duplicates
+	biglist.sort()
+	last = biglist[-1]
+	for i in range(len(biglist)-2, -1, -1):
+		if last == biglist[i]:
+			del biglist[i]
+		else:
+			last = biglist[i]
+	
+	#Remove entries older than 2 weeks
+	curtime = time.time()
+	twoweeksago = curtime - 1209600
+	for i in range(len(biglist)):
+		if int(biglist[i][1]) < twoweeksago:
+			del biglist[i]
+		
 
 def do_todo():
 	"""Gets a TODO list and maps their uids"""
